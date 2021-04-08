3a91635c25055deb35ec279b0c82e29cee6b2497
flower <851192902@qq.com>
2021-04-08 16:06:52 +0800

    Revert "0.3.3 ？？？"
    This reverts commit 8544aa892ebf7bf48c53afc21e0c647316b94c78.

--- src/main/java/com/xj/groupbuy/controller/manager/TrainController.java
+++ src/main/java/com/xj/groupbuy/controller/manager/TrainController.java
@@ -1,17 +1,9 @@
 package com.xj.groupbuy.controller.manager;
 
-<<<<<<< HEAD
 
 import com.xj.groupbuy.common.util.FileUtil;
 import com.xj.groupbuy.common.vo.CommonVO;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
-import com.xj.groupbuy.common.properties.FileProperties;
-import com.xj.groupbuy.common.util.FileUtil;
-import com.xj.groupbuy.common.vo.CommonVO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
->>>>>>> parent of 34aff61 (0.3.1 +文件上传功能基本完成)
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +17,6 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("manager/train")
 public class TrainController {
 
-<<<<<<< HEAD
 //    @Autowired
 //    private FileProperties fileProperties;
 //
@@ -44,18 +35,4 @@ public class TrainController {
 //        Page<StaffTrain> paramPage = new Page<>(pageNo,pageSize);
 //        return staffTrainService.page(paramPage, null);
 //    }
-=======
-    @Autowired
-    private FileProperties fileProperties;
-
-    @RequestMapping("upload")
-    public CommonVO fileUpload(@RequestParam("file") MultipartFile multipartFile){
-
-        if(multipartFile==null)
-            return new CommonVO(false,"文件上传失败");
-        boolean upload = FileUtil.upload(multipartFile, fileProperties.getPath(), multipartFile.getOriginalFilename());
-        
-        return new CommonVO(upload,upload?"上传成功":"上传失败");
-    }
->>>>>>> parent of 34aff61 (0.3.1 +文件上传功能基本完成)
 }
